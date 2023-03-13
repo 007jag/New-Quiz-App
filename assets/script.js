bc8e78f349cec3ebd8a242e8abe6098b4382647c
jag cheema <jagmitsingh2003@gmail.com>
2023-03-13 19:46:43 -0400

    added functionality for continue and start button

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,26 @@ var counter = 60;
 var score = 0;
 
 
+//If Continue Button Clicked
+continueBtn.onclick = ()=>{
+    infoBox.classList.add("hide");
+    startScreen.classList.remove("hide");
+};
+
+//If Start Button Clicked
+startBtn.onclick = () => {
+    function countdown(){
+        counter--;
+            if (counter === 0){
+                clearInterval(startCountdown)
+                quizEnd()
+            };
+    let timeRem = document.querySelector("#time-rem");
+    let timeTag = "<span>Time Left: "+ counter +"</span>"
+    timeRem.innerHTML = timeTag;
+    };
+    var startCountdown = setInterval(countdown, 1000);
+    startScreen.classList.add("hide");
+    quizBox.classList.remove("hide");
+    showQuestions(queCount)
+};
