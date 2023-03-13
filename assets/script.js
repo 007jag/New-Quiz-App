ed2919f9496784937776c0f4d1bc64525c246793
jag cheema <jagmitsingh2003@gmail.com>
2023-03-13 19:50:05 -0400

    added functionality to show questions being pulled from questions array

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,23 @@ startBtn.onclick = () => {
     quizBox.classList.remove("hide");
     showQuestions(queCount)
 };
+
+//Get Questions and Options from Array
+function showQuestions(index){
+    if (queCount>=10){
+        return;
+    }
+    const queText = document.querySelector(".que-text");
+    const optionList = document.querySelector("#choices");
+    let queTag = "<span>"+ questions[index].numb + ". "+ questions[index].question +"</span>";
+    let optionTag = '<div class="option">'+ questions[index].options[0] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[3] + '<span></span></div>'
+    queText.innerHTML = queTag;
+    optionList.innerHTML = optionTag;
+    const option = optionList.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
